be8bfc524bed83fd7a98ab601c06f88285e4196f
js-montgomery <js12154@nyu.edu>
2025-03-03 20:04:30 -0500

    flask route continued

--- webApp.py
+++ webApp.py
@@ -15,14 +15,64 @@ connection = pymongo.MongoClient("mongodb+srv://lgl1876523678:1017@cluster0.k8xw
 db = connection['project2_db']
 
 # flask route
-@app.route('/login')
+@app.route('/')
 def login():
+
     return render_template('login.html')
 
 @app.route('/register')
 def register():
+
     return render_template('newUser.html')
+
+@app.route('/handleRegister', method=['POST'])
+def handleRegister():
+    # store new user info in the database
+
+    # redirect to displayAll page
+    return redirect(url_for('displayALL'))
+
+@app.route('/search')
+def search():
+
+    return render_template('search.html')
+
+@app.route('/handleSearch')
+def handleSearch():
+    # process the search conditions
+
+    # redirect to displayAll page with conditions
+    return redirect(url_for('displayAll'))
+
+@app.route('/newData')
+def newData():
+
+    return render_template('newData.html')
+
+@app.route('/handleNewData', method=['POST'])
+def handleNewData():
+    # process the new data
+
+    # redirect to displayAll page with new data
+    return redirect(url_for('displayAll'))
+
+@app.route('/newGoal')
+def newGoal():
+    
+    return render_template('newGoal.html')
+
+@app.route('/handleNewGoal', method=['POST'])
+def handleNewGoal():
+    # process the new goal
+
+    # redirect to displayAll page with new goal
+    return redirect(url_for('displayAll'))
 
 @app.route('/displayAll')
 def displayAll():
-    return render_template('displayAll.html')
+    # check for search condition
+    # if no search condition, display all data
+    # if there is condition, filter the results
+
+    # redirect to displayAll page
+    return redirect(url_for('displayAll.html'))
